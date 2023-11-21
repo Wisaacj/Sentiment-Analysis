d89020ab7981f3d85c81d58319396c11e706a303
Wisaacj <Wisaacj@live.co.uk>
2023-11-21 17:13:45 +0000

    Placed `BinaryClassifierEvaluator` in a Python model

---
+++ evaluation.py
@@ -0,0 +1,91 @@
+import numpy as np
+import numpy.typing as npt
+
+
+class ClassifierPerformanceSummary:
+
+    def __init__(self, accuracy: float, precision: float, recall: float, f1: float):
+        self.accuracy = accuracy
+        self.precision = precision
+        self.recall = recall
+        self.f1 = f1
+
+    def __repr__(self) -> str:
+        return f"""
+        Accuracy:  {self.accuracy*100:.4f}%
+        Precision: {self.precision*100:.4f}%
+        Recall:    {self.recall*100:.4f}%
+        F1:        {self.f1*100:.4f}%
+        """
+    
+    def as_dict(self) -> dict:
+        return {
+            'accuracy': self.accuracy,
+            'precision': self.precision,
+            'recall': self.recall,
+            'f1': self.f1
+        }
+
+
+class BinaryClassifierEvaluator:
+
+    def __init__(self, ground_truth: npt.NDArray, predictions: npt.NDArray):
+        self.predictions = predictions
+        self.ground_truth = ground_truth
+        self.confusion_matrix = self.produce_confusion_matrix()
+        """
+        [[ TP, FP ],
+         [ FN, TN ]]
+        """
+
+    def produce_confusion_matrix(self) -> npt.NDArray:
+        confusion_matrix = np.zeros(shape=(2, 2))
+
+        # True positives: where both prediction and ground truth are 1.
+        confusion_matrix[0, 0] = np.sum((self.predictions == 1) & (self.ground_truth == 1))
+
+        # False positives: where prediction is 1, but ground truth is 0.
+        confusion_matrix[0, 1] = np.sum((self.predictions == 1) & (self.ground_truth == 0))
+        
+        # False negatives: where prediction is 0, but ground truth is 1.
+        confusion_matrix[1, 0] = np.sum((self.predictions == 0) & (self.ground_truth == 1))
+        
+        # True negatives: where both prediction and ground truth are 0.
+        confusion_matrix[1, 1] = np.sum((self.predictions == 0) & (self.ground_truth == 0))
+
+        return confusion_matrix.astype(int)
+
+    def calculate_accuracy(self) -> float:
+        """
+        Calculates (TP + TN) / Num Predictions.
+        """
+        return (self.confusion_matrix[0, 0] + self.confusion_matrix[1, 1]) / self.confusion_matrix.sum()
+
+    def calculate_recall(self) -> float:
+        """
+        Calculates TP / (TP + FN)
+        """
+        return self.confusion_matrix[0, 0] / self.confusion_matrix[:, 0].sum()
+
+    def calculate_precision(self) -> float:
+        """
+        Calculates TP / (TP + FP).
+        """
+        return self.confusion_matrix[0, 0] / self.confusion_matrix[0, :].sum()
+    
+    def calculate_f1(self) -> float:
+        """
+        Calculates the harmonic mean of precision and recall.
+        """
+        precision = self.calculate_precision()
+        recall = self.calculate_recall()
+
+        return 2 * (precision * recall) / (precision + recall)
+    
+    def get_summary(self) -> ClassifierPerformanceSummary:
+        return ClassifierPerformanceSummary(
+            self.calculate_accuracy(),
+            self.calculate_precision(),
+            self.calculate_recall(),
+            self.calculate_f1()
+        )
