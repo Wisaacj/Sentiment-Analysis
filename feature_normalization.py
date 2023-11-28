237f41f0718bffd51b5d304632cc849df3c89074
Wisaacj <Wisaacj@live.co.uk>
2023-11-28 17:10:42 +0000

    Remove least informative features

--- feature_normalization.py
+++ feature_normalization.py
@@ -1,6 +1,8 @@
 import copy
 import math
+from typing import Tuple
 import numpy as np
+import numpy.typing as npt
 from collections import Counter, defaultdict
 
 from dataset_types import FeatureSet
@@ -15,26 +17,23 @@ class FeatureSetNormalizer:
         self.normalized = False
         self.shared_vocabulary = self._collect_shared_vocabulary()
 
-    def perform_tf(self) -> FeatureSet:
-        self._calculate_term_frequencies()
+        self.num_samples = len(self.feature_set)
+        self.num_features = len(self.shared_vocabulary)
 
-        for datapoint in self.feature_set:
-            datapoint.contents = [
-                datapoint.term_frequencies.get(token, 0)
-                for token in self.shared_vocabulary
-            ]
+    def perform_tf_norm(self, drop_percentile: float = 0) -> FeatureSet:
+        _, tf_matrix = self._calculate_tf_idf_scores(drop_percentile)
+
+        for doc_idx, datapoint in enumerate(self.feature_set):
+            datapoint.contents = tf_matrix[doc_idx, :]
 
         return self.feature_set
 
-    def perform_tf_idf(self) -> FeatureSet:
-        self._calculate_term_frequencies()
-        self.idfs = self._calculate_idfs()
+    def perform_tf_idf_norm(self, drop_percentile: float = 0) -> FeatureSet:
+        tfidf_matrix, _ = self._calculate_tf_idf_scores(drop_percentile)
 
-        for datapoint in self.feature_set:
-            datapoint.contents = [
-                (datapoint.term_frequencies.get(token, 0) * self.idfs.get(token))
-                for token in self.shared_vocabulary
-            ]
+        # Update datapoint contents with tf-idf values
+        for doc_idx, datapoint in enumerate(self.feature_set):
+            datapoint.contents = tfidf_matrix[doc_idx, :]
 
         return self.feature_set
 
@@ -42,77 +41,68 @@ class FeatureSetNormalizer:
         raise NotImplementedError
 
     def _collect_shared_vocabulary(self) -> set:
-        return set(sorted({
+        return {
             token
             for datapoint in self.feature_set
             for token in datapoint.contents
-        }))
-    
-    def _calculate_idfs(self) -> dict:
-        # We calculate the document frequencies by creating a unique set of tokens for
-        # each datapoint (i.e., for each document in the set, counting each token once
-        # per document regardless of its frequency within the document itself). The Counter
-        # then aggregates these sets across all datapoints, counting the number of documents
-        # in which each distinct token appears. This gives us the document frequency for
-        # each term in the `shared_vocabulary`.
-        document_frequencies = Counter(
-            token
-            for datapoint in self.feature_set
-            for token in set(datapoint.contents)
-        )
-        num_datapoints = len(self.feature_set)
-
-        return {
-            token: math.log(num_datapoints / (doc_frequency + 1))
-            for token, doc_frequency in document_frequencies.items()
         }
     
-    def _calculate_term_frequencies(self) -> None:
-        for datapoint in self.feature_set:
-            num_tokens = len(datapoint.contents)
-            term_occurences = Counter(datapoint.contents)
-            
-            # Normalise the term occurences by dividing them by the length of the datapoint.
-            datapoint.term_frequencies = {
-                token: count / num_tokens
-                for token, count in term_occurences.items()
-            }
+    def _remove_rare_features(self, tfidf_matrix: npt.NDArray, tf_matrix: npt.NDArray, drop_percentile: float) -> Tuple[npt.NDArray, npt.NDArray]:
+        total_tfidf_per_feature = np.sum(tfidf_matrix, axis=0)
+        total_tfidf = np.sum(total_tfidf_per_feature)
 
-    def perform_fast_tf_idf(self) -> FeatureSet:
+        # Get the indices of total_tfidf_per_feature if it were sorted.
+        sorted_indices = np.argsort(total_tfidf_per_feature)
+        # Calculate the cumulative sum along the sorted features.
+        sorted_cumulative_tfidf = np.cumsum(total_tfidf_per_feature[sorted_indices])
+
+        # Determine the cut-off index where the cumulative sum reaches the threshold
+        # percentage.
+        threshold_index = np.searchsorted(sorted_cumulative_tfidf, drop_percentile * total_tfidf)
+
+        # Use the threshold_index to determine the indices of features to keep.
+        features_to_keep_indices = sorted_indices[threshold_index:]
+
+        # Keep only the columns for features we want to retain.
+        tfidf_matrix = tfidf_matrix[:, features_to_keep_indices]
+        tf_matrix = tf_matrix[:, features_to_keep_indices]
+
+        return tfidf_matrix, tf_matrix
+    
+    def _calculate_tf_idf_scores(self, drop_percentile: float) -> Tuple[npt.NDArray, npt.NDArray]:
         vocab_to_index = {word: idx for idx, word in enumerate(self.shared_vocabulary)}
-        num_documents = len(self.feature_set)
-        num_vocab = len(self.shared_vocabulary)
-        
-        # TF matrix where each row corresponds to a document, and each column corresponds to a term.
-        tf_matrix = np.zeros(shape=(num_documents, num_vocab), dtype=float)
-        
-        # Document frequency (DF) counter for counting in how many documents a term appears.
+
+        # Term frequency matrix. Each row corresponds to a document and each column to a term.
+        tf_matrix = np.zeros(shape=(self.num_samples, self.num_features), dtype=float)
+        # This will store a count of how many documents each term appears in, defaulting to 0.
         df_counter = defaultdict(int)
-        
-        # Populate TF matrix and DF counter
+
+        # Populate tf_matrix and df_counter
         for doc_idx, datapoint in enumerate(self.feature_set):
-            # Count term occurrences in the document
-            term_occurrences = Counter(datapoint.contents)
-            for term, count in term_occurrences.items():
+            # Count term occurences in this document.
+            term_occurences = Counter(datapoint.contents)
+            for term, count in term_occurences.items():
                 if term in vocab_to_index:
                     index = vocab_to_index[term]
-                    tf_matrix[doc_idx, index] = count  # Raw count for TF (to be normalized later)
+                    # Raw count for TF (to be normalised later)
+                    tf_matrix[doc_idx, index] = count
+                    # Increment the df counter.
                     df_counter[term] += 1
-        
-        # Normalize TF matrix row-wise (divide by the number of terms in each document)
-        doc_lengths = np.array([len(dp.contents) for dp in self.feature_set])
-        tf_matrix = tf_matrix / doc_lengths[:, None]  # Broadcasting division
-        
-        # Convert the DF counter into an array of IDF values
-        # idf_array = np.log((1 + num_documents) / (1 + np.array([df_counter[term] for term in self.shared_vocabulary]))) + 1
-        idf_array = np.log((num_documents) / (1 + np.array([df_counter[term] for term in self.shared_vocabulary])))
-        
-        # TF-IDF calculation by multiplying the TF matrix by the IDF values
-        # The transpose on idf_array is necessary for broadcasting to correct dimension
-        tfidf_matrix = tf_matrix * idf_array
-        
-        # Update datapoint contents with tf-idf values
-        for doc_idx, datapoint in enumerate(self.feature_set):
-            datapoint.contents = tfidf_matrix[doc_idx, :] # .tolist()
 
-        return self.feature_set
+        # Normalise the term frequency matrix row-wise (divide by the number of terms in each document).
+        doc_lengths = np.array([len(datapoint.contents) for datapoint in self.feature_set])
+        tf_matrix = tf_matrix / doc_lengths[:, None]
+
+        # Transform document frequencies into inverse-document frequencies.
+        idf_array = np.log(
+            (self.num_samples) / (1 + np.array([df_counter[term] for term in self.shared_vocabulary]))
+        )
+
+        # Calculate the TF-IDF matrix by multily the TF matrix by the IDF values.
+        tfidf_matrix = tf_matrix * idf_array
+
+        if drop_percentile > 0:
+            # Remove features that appear most infrequently.
+            tfidf_matrix, tf_matrix = self._remove_rare_features(tfidf_matrix, tf_matrix, drop_percentile)
+
+        return tfidf_matrix, tf_matrix
